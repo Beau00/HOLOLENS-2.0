9631f6d106216618338a80062edd5fa595ad74a9
Aithusa1 <77977074+Aithusa1@users.noreply.github.com>
2022-12-14 14:43:25 +0100

    cloud hover

--- Hololens2.0/Assets/Scripts/Weather System/Clouds.cs
+++ Hololens2.0/Assets/Scripts/Weather System/Clouds.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Clouds : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector2 range = new Vector2(2, 4);
+    public float speed = 0.5f;
+    public Vector3 direction = Vector3.right;
+    private Vector3 startPosition;
+
+    private bool handInteraction = false;
+
     void Start()
     {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (!handInteraction)
+        {
+            Vector3 newPosition = startPosition + direction * Mathf.PingPong(Time.time * speed, range.y - range.x) + range.x * direction;
+            transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime);
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
